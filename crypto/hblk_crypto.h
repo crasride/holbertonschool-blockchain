ac549268a2091e099277ceedbaed381b94b236f3
crasride <jose.dfernandez@yahoo.fr>
2024-02-27 11:48:45 +0100

    task 3

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -46,4 +46,7 @@ EC_KEY *ec_create(void);
 /*Function Tasks 2*/
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 
+/*Function Tasks 3*/
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+
 #endif /* _CRYPTO_H_ */
