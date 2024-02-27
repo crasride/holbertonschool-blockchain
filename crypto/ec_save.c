e2289809525b4217d45071ca5d8b09d4b96f843f
crasride <jose.dfernandez@yahoo.fr>
2024-02-27 12:30:09 +0100

    fix betty

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -16,49 +16,38 @@ int ec_save(EC_KEY *key, char const *folder)
 	/* Check if key or folder is NULL */
 	if (!key || !folder)
 		return (0);
+
 	/* Create the paths for private and public key files */
 	sprintf(path_priv, "%s/key.pem", folder);
 	sprintf(path_pub, "%s/key_pub.pem", folder);
+
 	/* Ensure the folder exists */
-	if (access(folder, F_OK) == -1)
+	if (access(folder, F_OK) == -1 && mkdir(folder, S_IRWXU | S_IRGRP | S_IXGRP
+												| S_IROTH | S_IXOTH) == -1)
 	{
-		if (mkdir(folder, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) == -1)
-		{
-			perror("mkdir");
-			return (0);
-		}
+		perror("mkdir");
+		return (0);
 	}
 	/* Save private key to file */
 	FILE *priv_file = fopen(path_priv, "w");
 
-	if (!priv_file)
+	if (!priv_file || PEM_write_ECPrivateKey(priv_file, key, NULL, NULL, 0,
+											NULL, NULL) == 0)
 	{
-		perror("fopen");
-		return (0);
-	}
-	if (PEM_write_ECPrivateKey(priv_file, key, NULL, NULL, 0, NULL, NULL) == 0)
-	{
-		fprintf(stderr, "Failed to write private key to file\n");
+		perror(!priv_file ? "fopen" : "Failed to write private key to file");
 		fclose(priv_file);
 		return (0);
 	}
-
 	fclose(priv_file);
 	/* Save public key to file */
 	FILE *pub_file = fopen(path_pub, "w");
 
-	if (!pub_file)
+	if (!pub_file || PEM_write_EC_PUBKEY(pub_file, key) == 0)
 	{
-		perror("fopen");
-		return (0);
-	}
-	if (PEM_write_EC_PUBKEY(pub_file, key) == 0)
-	{
-		fprintf(stderr, "Failed to write public key to file\n");
+		perror(!pub_file ? "fopen" : "Failed to write public key to file");
 		fclose(pub_file);
 		return (0);
 	}
 	fclose(pub_file);
-
 	return (1);
 }
