91c3eaa71a52c61330c9a01d35917b9ea0283fc8
crasride <jose.dfernandez@yahoo.fr>
2024-03-12 14:44:10 +0100

    task1

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -7,8 +7,10 @@
 #include <string.h>
 #include <llist.h>
 #include <openssl/sha.h> /* SHA256_DIGEST_LENGTH */
+#include <time.h>
 
 #define GENESIS_HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
+#define SHA256_DIGEST_LENGTH 32
 
 /* Constants */
 #define BLOCKCHAIN_DATA_MAX 1024
@@ -79,9 +81,12 @@ typedef struct blockchain_s
 } blockchain_t;
 
 
-
-/* Funtions tasks  */
+/* Functions tasks */
 blockchain_t *blockchain_create(void);
 
+/* Functions Task1 */
+block_t *block_create(block_t const *prev, int8_t const *data,
+						uint32_t data_len);
 
+/* __attribute__((warn_unused_result)); */
 #endif /* _BLOCKCHAIN_H_ */
