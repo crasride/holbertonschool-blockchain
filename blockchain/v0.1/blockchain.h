de1b3712e1280c2844372d0bc9929c5134e1ff88
crasride <jose.dfernandez@yahoo.fr>
2024-03-12 17:46:17 +0100

    Hash Block

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -12,6 +12,8 @@
 #define GENESIS_HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
 #define SHA256_DIGEST_LENGTH 32
 
+#include "../../crypto/hblk_crypto.h"
+
 /* Constants */
 #define BLOCKCHAIN_DATA_MAX 1024
 
@@ -93,6 +95,10 @@ void block_destroy(block_t *block);
 
 /* Functions Task3 */
 void blockchain_destroy(blockchain_t *blockchain);
+
+/* Functions Task4 */
+uint8_t *block_hash(block_t const *block,
+										uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
 
 /* __attribute__((warn_unused_result)); */
 #endif /* _BLOCKCHAIN_H_ */
