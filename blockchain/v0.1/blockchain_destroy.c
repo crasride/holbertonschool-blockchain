33ba49749d293dc14c715a43e0771d68cfb2ca4a
crasride <jose.dfernandez@yahoo.fr>
2024-03-12 15:49:45 +0100

    task Delete Blockchain

---
+++ blockchain/v0.1/blockchain_destroy.c
@@ -0,0 +1,14 @@
+#include "blockchain.h"
+
+/**
+* blockchain_destroy - Deletes an existing Blockchain and all its Blocks
+* @blockchain: Pointer to the Blockchain structure to delete
+*/
+void blockchain_destroy(blockchain_t *blockchain)
+{
+	if (!blockchain)
+		return;
+
+	llist_destroy(blockchain->chain, 1, NULL);
+	free(blockchain);
+}
