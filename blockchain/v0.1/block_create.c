b977005686984fc72617ffa34e62d7607638a125
crasride <jose.dfernandez@yahoo.fr>
2024-03-12 14:57:41 +0100

    task1-0.1

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -1,39 +1,33 @@
 #include "blockchain.h"
 
 /**
- * block_create - Creates a Block structure and initializes it
- * @prev: Pointer to the previous Block in the Blockchain
- * @data: Points to a memory area to duplicate in the Block’s data
- * @data_len: Number of bytes to duplicate in data
- *
- * Return: A pointer to the allocated Block
- */
+* block_create - Creates a Block structure and initializes it
+* @prev: Pointer to the previous Block in the Blockchain
+* @data: Points to a memory area to duplicate in the Block’s data
+* @data_len: Number of bytes to duplicate in data
+*
+* Return: A pointer to the allocated Block
+*/
 block_t *block_create(block_t const *prev, int8_t const *data,
 															uint32_t data_len)
 {
-	block_t *new_block = malloc(sizeof(block_t));
+	block_t *block;
+	uint32_t max_len = data_len > BLOCKCHAIN_DATA_MAX ?
+		BLOCKCHAIN_DATA_MAX : data_len;
 
-	if (!new_block)
+	if (!prev || !data)
+		return (NULL);
+	block = calloc(1, sizeof(*block));
+	if (!block)
 		return (NULL);
 
-	new_block->info.index = prev->info.index + 1;
-	new_block->info.difficulty = 0;
-	new_block->info.nonce = 0;
-	new_block->info.timestamp = time(NULL);
-	memset(new_block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
+	memcpy(block->data.buffer, data, max_len);
+	block->data.len = max_len;
 
-	if (prev)
-		memcpy(new_block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
-
-	if (data_len > BLOCKCHAIN_DATA_MAX)
-		data_len = BLOCKCHAIN_DATA_MAX;
-
-	memcpy(new_block->data.buffer, data, data_len);
-	new_block->data.len = data_len;
-
-	memset(new_block->hash, 0, SHA256_DIGEST_LENGTH);
-
-	return (new_block);
+	memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
+	block->info.index = prev->info.index + 1;
+	block->info.timestamp = (uint64_t)time(NULL);
+	return (block);
 }
 
 
