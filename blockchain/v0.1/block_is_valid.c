3de8f92c0ef4907c9415b26e94eda5bee897ac74
crasride <jose.dfernandez@yahoo.fr>
2024-03-13 14:27:28 +0100

    Block validity

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -1,6 +1,17 @@
 #include "blockchain.h"
 
 /**
+* cleanup_hash - Clears a hash by setting all its bytes to 0
+* @hash: Pointer to the hash to clear
+* @hash_len: Length of the hash in bytes
+*/
+void cleanup_hash(uint8_t *hash, size_t hash_len)
+{
+	memset(hash, 0, hash_len);
+}
+
+
+/**
 * block_is_valid - Verifies if a block is valid
 * @block: Pointer to the block to check
 * @prev_block: Pointer to the previous block in the blockchain
@@ -9,28 +20,30 @@
 */
 int block_is_valid(block_t const *block, block_t const *prev_block)
 {
-	block_t genesis = GENESIS_BLOCK;
+	uint8_t hash_verify[SHA256_DIGEST_LENGTH] = {0};
+	block_t const genesis = GENESIS_BLOCK;
 
-	if (block == NULL)
+	if (!block || (!prev_block && block->info.index != 0))
 		return (0);
 
-	if (prev_block == NULL)
-	{
-		if (block->info.index != 0)
-			return (0);
+	if (block->info.index == 0)
+		return (memcmp(block, &genesis, sizeof(genesis)) == 0 ? 1 : 0);
 
-		if (memcmp(block, &genesis, sizeof(block_t)) != 0)
-			return (0);
-	}
-	else
-	{
-		if (block->info.index != prev_block->info.index + 1 ||
-			memcmp(block->info.prev_hash, prev_block->hash, SHA256_DIGEST_LENGTH) != 0)
-			return (0);
-	}
+	if (block->info.index != prev_block->info.index + 1)
+		return (0);
 
-	if (block_hash(block, NULL) == NULL ||
-		memcmp(block->hash, block->info.hash, SHA256_DIGEST_LENGTH) != 0)
+	if (block_hash(prev_block, hash_verify) == NULL ||
+		memcmp(hash_verify, prev_block->hash, SHA256_DIGEST_LENGTH) != 0)
+		return (0);
+
+	if (memcmp(prev_block->hash, block->info.prev_hash,
+													SHA256_DIGEST_LENGTH) != 0)
+		return (0);
+
+	cleanup_hash(hash_verify, SHA256_DIGEST_LENGTH);
+
+	if (block_hash(block, hash_verify) == NULL ||
+		memcmp(hash_verify, block->hash, SHA256_DIGEST_LENGTH) != 0)
 		return (0);
 
 	if (block->data.len > BLOCKCHAIN_DATA_MAX)
