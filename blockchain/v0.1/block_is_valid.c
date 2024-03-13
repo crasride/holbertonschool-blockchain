64bf62bf522f1ce2d6567154dc88b7e8a8499533
crasride <jose.dfernandez@yahoo.fr>
2024-03-13 14:37:21 +0100

    Block validity 2

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -27,12 +27,12 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 		return (1);
 
 	if (block->info.index == 0)
-		return (memcmp(block, &genesis, sizeof(genesis)) == 0 ? 1 : 0);
+		return (memcmp(block, &genesis, sizeof(genesis)));
 
 	if (block->info.index != prev_block->info.index + 1)
 		return (1);
 
-	if (block_hash(prev_block, hash_verify) == NULL ||
+	if (block_hash(prev_block, hash_verify) ||
 		memcmp(hash_verify, prev_block->hash, SHA256_DIGEST_LENGTH) != 0)
 		return (1);
 
@@ -42,7 +42,7 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 
 	cleanup_hash(hash_verify, SHA256_DIGEST_LENGTH);
 
-	if (block_hash(block, hash_verify) == NULL ||
+	if (!block_hash(block, hash_verify) ||
 		memcmp(hash_verify, block->hash, SHA256_DIGEST_LENGTH) != 0)
 		return (1);
 
