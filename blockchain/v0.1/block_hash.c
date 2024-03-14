c6c091bb282ade084f39bf7a450c3f1e2264531b
crasride <jose.dfernandez@yahoo.fr>
2024-03-14 17:06:28 +0100

    Hash Block

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -8,10 +8,13 @@
 * Return: Pointer to hash_buf
 */
 uint8_t *block_hash(block_t const *block,
-		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+										uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
+	size_t len;
+
 	if (!block)
 		return (NULL);
-	return (sha256((int8_t const *)block,
-		    sizeof(block->info) + block->data.len, hash_buf));
+
+	len = sizeof(block->info) + block->data.len;
+	return (sha256((int8_t const *)block, len, hash_buf));
 }
