77547974c1f9ae0e61e51d5baac1523e98712d15
crasride <jose.dfernandez@yahoo.fr>
2024-03-25 16:43:17 +0100

    prototypes functions

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -82,4 +82,7 @@ unspent_tx_out_t *unspent_tx_out_create(
 	uint8_t block_hash[SHA256_DIGEST_LENGTH],
 	uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out);
 
+/* Functions Task 2*/
+tx_in_t *tx_in_create(unspent_tx_out_t const *unspent);
+
 #endif /* _TRANSACTION_H_ */
