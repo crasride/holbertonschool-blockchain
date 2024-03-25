518924e90744a610c1a75cafb068782c22c28f70
crasride <jose.dfernandez@yahoo.fr>
2024-03-25 15:15:58 +0100

    ver

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -1,20 +1,32 @@
 #include "transaction.h"
 
 /**
- * tx_out_create - creates a new transaction output structure
- * @amount: the transaction amount
- * @pub: public key of receiver
- * Return: pointer to new structure or NULL
- */
+* tx_out_create - Creates a transaction output structure
+* @amount: Amount in the transaction output
+* @pub: Receiver's public address
+*
+* Return: A pointer to the allocated transaction output
+*/
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
-	tx_out_t *t = calloc(1, sizeof(*t));
+	tx_out_t *output;
 
-	if (!t)
+	if (pub == NULL)
 		return (NULL);
-	t->amount = amount;
-	memcpy(t->pub, pub, sizeof(t->pub));
-	if (!sha256((int8_t const *)t, sizeof(t->amount) + sizeof(t->pub), t->hash))
-		return (free(t), NULL);
-	return (t);
+
+	output = malloc(sizeof(tx_out_t));
+	if (output == NULL)
+		return (NULL);
+
+	output->amount = amount;
+	memcpy(output->pub, pub, EC_PUB_LEN);
+
+	/* Compute the hash of the transaction output */
+	if (!sha256((const int8_t *)output, sizeof(tx_out_t), output->hash))
+	{
+		free(output);
+		return (NULL);
+	}
+
+	return (output);
 }
