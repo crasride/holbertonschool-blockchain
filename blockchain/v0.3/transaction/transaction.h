659d8804bf8dfad320d2b4c3af48c38ba595ad11
crasride <jose.dfernandez@yahoo.fr>
2024-03-25 16:37:56 +0100

    prototypes functions

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -77,4 +77,9 @@ typedef struct unspent_tx_out_s
 /* Functions Task 0*/
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
 
+/* Functions Task 1*/
+unspent_tx_out_t *unspent_tx_out_create(
+	uint8_t block_hash[SHA256_DIGEST_LENGTH],
+	uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out);
+
 #endif /* _TRANSACTION_H_ */
