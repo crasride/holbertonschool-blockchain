42a823ce2c3467cbe777b436f4d2153b6823d3e5
crasride <jose.dfernandez@yahoo.fr>
2024-03-25 16:45:49 +0100

    task2

--- blockchain/v0.3/transaction/tx_in_create.c
+++ blockchain/v0.3/transaction/tx_in_create.c
@@ -10,10 +10,12 @@
 */
 tx_in_t *tx_in_create(unspent_tx_out_t const *unspent)
 {
+	tx_in_t *input;
+
 	if (!unspent)
 		return (NULL);
 
-	tx_in_t *input = calloc(1, sizeof(*input));
+	input = calloc(1, sizeof(*input));
 
 	if (!input)
 		return (NULL);
@@ -21,9 +23,6 @@ tx_in_t *tx_in_create(unspent_tx_out_t const *unspent)
 	memcpy(input->block_hash, unspent->block_hash, sizeof(input->block_hash));
 	memcpy(input->tx_id, unspent->tx_id, sizeof(input->tx_id));
 	memcpy(input->tx_out_hash, unspent->out.hash, sizeof(input->tx_out_hash));
-
-	/* Zero out the signature */
-	memset(&input->sig, 0, sizeof(input->sig));
 
 	return (input);
 }
