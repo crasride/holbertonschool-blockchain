4e5191c75c8935438f87198b1a53da1219598eb2
crasride <jose.dfernandez@yahoo.fr>
2024-03-26 16:01:26 +0100

    task6 check9.2

--- blockchain/v0.3/transaction/transaction_is_valid.c
+++ blockchain/v0.3/transaction/transaction_is_valid.c
@@ -30,31 +30,31 @@ int select_unspent_transaction(llist_node_t node, unsigned int idx, void *args)
 	* printf("ptr[2] (input transaction id): %u\n", *(uint32_t *)ptr[2]);
 	* printf("ptr[3] (transaction ID): %p\n", ptr[3]);
 	*/
-	(void)idx;/* Avoid the warning "unused parameter" */
-	/* Check if the unspent transaction matches the transaction input */
+	 (void)idx; /* Avoid the warning "unused parameter" */
+
 	if (!tx_in)
-		return (1);
+		return (1); /* No matching transaction input found */
 
 	if (!memcmp(unspent->out.hash, tx_in->tx_out_hash, SHA256_DIGEST_LENGTH))
 	{
-		/* Increment the index and balance */
-		*tx_in_idx += 1;
-		/* Update the balance */
-		*balance += unspent->out.amount;
-		/* Create a new transaction input */
-		key = ec_from_pub(unspent->out.pub);
-		/* Check if the public key is valid */
+		*tx_in_idx += 1; /* Increment the index */
+		*balance += unspent->out.amount; /* Update the balance */
+
+		key = ec_from_pub(unspent->out.pub); /* Create a new transaction input */
 		if (!key)
-			return (1);
-		/* Verify the signature of the transaction input */
+			return (1); /* Error creating the public key */
+
 		if (!ec_verify(key, ptr[3], SHA256_DIGEST_LENGTH, &tx_in->sig))
 		{
 			EC_KEY_free(key);
-			return (1);
+			return (1); /* Signature verification failed */
 		}
+
 		EC_KEY_free(key);
+		return (0); /* Matching unspent transaction found */
 	}
-	return (0);
+
+	return (1); /* Unspent output referred to by transaction input not found */
 }
 
 /**
