7530ea139428497f88fc206550e94c60fb40fb26
crasride <jose.dfernandez@yahoo.fr>
2024-03-27 11:09:03 +0100

    task11.1

--- blockchain/v0.3/blockchain_destroy.c
+++ blockchain/v0.3/blockchain_destroy.c
@@ -8,7 +8,7 @@ void blockchain_destroy(blockchain_t *blockchain)
 {
 	if (!blockchain)
 		return;
-	llist_destroy(blockchain->chain, 1, (void (*)(llist_node_t))block_destroy);
-	llist_destroy(blockchain->unspent, 1, free);
+	
+	llist_destroy(blockchain->chain, 1, NULL);
 	free(blockchain);
 }
