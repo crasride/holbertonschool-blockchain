03d6c899513eeb9f83496674d906cba30815bfea
crasride <jose.dfernandez@yahoo.fr>
2024-03-27 10:55:20 +0100

    task10.4

--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -1,12 +1,13 @@
 #include "blockchain.h"
 
 /**
-* block_destroy - Deletes an existing Block
-* @block: Pointer to the Block to delete
+* block_destroy - frees a block
+* @block: pointer to block to free
 */
 void block_destroy(block_t *block)
 {
+	if (!block)
+		return;
 	llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
-	/* Free the block */
 	free(block);
 }
