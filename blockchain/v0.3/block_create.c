073a7fff4d22bc5c818a8523bfb7f5824056ed57
crasride <jose.dfernandez@yahoo.fr>
2024-03-27 09:19:54 +0100

    task10

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -1,45 +1,25 @@
 #include "blockchain.h"
 
 /**
- * block_create - Creates a Block structure and initializes it
- * @prev: Pointer to the previous Block in the Blockchain
- * @data: Points to a memory area to duplicate in the Block’s data
- * @data_len: Number of bytes to duplicate in data
- *
- * Return: A pointer to the allocated Block
+ * block_create - creates a new block in the sequence
+ * @prev: pointer to previous block
+ * @data: pointer of data to duplicate
+ * @data_len: length of data
+ * Return: pointer to new block or NULL
  */
 block_t *block_create(block_t const *prev, int8_t const *data,
-					uint32_t data_len)
+	uint32_t data_len)
 {
-	block_t *block; /* Block to create */
-	uint32_t max_len; /* Maximum length of data to copy */
+	block_t *block = calloc(1, sizeof(*block));
+	llist_t *transactions = llist_create(MT_SUPPORT_FALSE);
 
-	/* Check if the previous block and data pointers are NULL */
-	if (!prev || !data)
-		return (NULL);
-
-	/* Allocate memory for the new block */
-	block = calloc(1, sizeof(*block));
-	if (!block)
-		return (NULL);
-
-	/* Initialize the Block's transaction list to an empty linked list */
-	block->transactions = llist_create(MT_SUPPORT_FALSE);
-	if (!block->transactions)
-	{
-		free(block);
-		return (NULL);
-	}
-
-	/* Set the block's information */
-	memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
+	if (!block || !transactions)
+		return (free(block), llist_destroy(transactions, 0, NULL), NULL);
+	memcpy(&(block->data.buffer), data, MIN(data_len, BLOCKCHAIN_DATA_MAX));
+	block->data.len = MIN(data_len, BLOCKCHAIN_DATA_MAX);
 	block->info.index = prev->info.index + 1;
-	block->info.timestamp = (uint64_t)time(NULL);
-
-	/* Copy the provided data to the block's data */
-	max_len = data_len > BLOCKCHAIN_DATA_MAX ? BLOCKCHAIN_DATA_MAX : data_len;
-	memcpy(block->data.buffer, data, max_len);
-	block->data.len = max_len;
-
+	block->info.timestamp = time(NULL);
+	block->transactions = transactions;
+	memcpy(&(block->info.prev_hash), prev->hash, SHA256_DIGEST_LENGTH);
 	return (block);
 }
