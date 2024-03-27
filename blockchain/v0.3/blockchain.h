caf52ffc9fe9aa3f4e21e02610ff719e86722a24
crasride <jose.dfernandez@yahoo.fr>
2024-03-27 15:10:22 +0100

    task13

--- blockchain/v0.3/blockchain.h
+++ blockchain/v0.3/blockchain.h
@@ -132,20 +132,23 @@ void cleanup_block_data(block_t *block, llist_t *list);
 /* Functions Task7 */
 #define GENESIS_BLOCK { \
 	{ /* info */ \
-		0, /* index */ \
+		0 /* index */, \
 		0, /* difficulty */ \
 		1537578000, /* timestamp */ \
 		0, /* nonce */ \
-		{0}, /* prev_hash */ \
+		{0} /* prev_hash */ \
 	}, \
 	{ /* data */ \
 		"Holberton School", /* buffer */ \
 		16 /* len */ \
 	}, \
+	NULL, /* transactions */ \
 	"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d" \
 	"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03" \
 }
-int block_is_valid(block_t const *block, block_t const *prev_block);
+
+int block_is_valid(block_t const *block, block_t const *prev_block,
+														llist_t *all_unspent);
 
 /*Proyect Blockchain - Transactions*/
 /* Functions Task0 */
