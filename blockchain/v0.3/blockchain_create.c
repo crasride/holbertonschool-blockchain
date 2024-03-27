269147735d74742d884c0e840d081032bcdc1964
crasride <jose.dfernandez@yahoo.fr>
2024-03-27 11:22:47 +0100

    task11.3

--- blockchain/v0.3/blockchain_create.c
+++ blockchain/v0.3/blockchain_create.c
@@ -11,19 +11,22 @@ blockchain_t *blockchain_create(void)
 	blockchain_t *blockchain = malloc(sizeof(blockchain_t));
 	/* Allocate memory for the genesis block */
 	block_t *genesis_block = malloc(sizeof(block_t));
-
 	/* If a memory allocation fails, return NULL */
 	if (!blockchain)
 		return (NULL);
-
-	/* Initialize the blockchain */
 	blockchain->chain = llist_create(MT_SUPPORT_TRUE);
 	if (!blockchain->chain)
 	{
 		free(blockchain);
 		return (NULL);
 	}
-	/* Initialize the genesis block */
+	blockchain->unspent = llist_create(MT_SUPPORT_FALSE);
+	if (!blockchain->unspent)
+	{
+		llist_destroy(blockchain->chain, 1, NULL);
+		free(blockchain);
+		return (NULL);
+	}
 	if (!genesis_block)
 	{
 		free(blockchain->chain);
@@ -35,9 +38,7 @@ blockchain_t *blockchain_create(void)
 	genesis_block->info.difficulty = 0;
 	genesis_block->info.timestamp = 1537578000;
 	genesis_block->info.nonce = 0;
-	/* Set the hash to the genesis hash */
 	memset(genesis_block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
-	/* Set the data to "Holberton School" */
 	memcpy(genesis_block->data.buffer, "Holberton School", 16);
 	/* Set the data length to 16 */
 	genesis_block->data.len = 16;
@@ -45,6 +46,5 @@ blockchain_t *blockchain_create(void)
 	memcpy(genesis_block->hash, GENESIS_HASH, SHA256_DIGEST_LENGTH);
 	/* Add the genesis block to the blockchain */
 	llist_add_node(blockchain->chain, genesis_block, ADD_NODE_FRONT);
-
 	return (blockchain);
 }
