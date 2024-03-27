f71b4f8e9c8ab92f8094ccb2ba31d69d3bb74261
crasride <jose.dfernandez@yahoo.fr>
2024-03-27 11:00:05 +0100

    task11

--- blockchain/v0.3/blockchain_destroy.c
+++ blockchain/v0.3/blockchain_destroy.c
@@ -1,14 +1,14 @@
 #include "blockchain.h"
 
 /**
- * blockchain_destroy - frees an entire chain of blocks
- * @blockchain: pointer to chain struct
- */
+* blockchain_destroy - Deletes an existing Blockchain and all its Blocks
+* @blockchain: Pointer to the Blockchain structure to delete
+*/
 void blockchain_destroy(blockchain_t *blockchain)
 {
 	if (!blockchain)
 		return;
-	llist_destroy(blockchain->chain, 1, (void (*)(llist_node_t))block_destroy);
-	llist_destroy(blockchain->unspent, 1, free);
+
+	llist_destroy(blockchain->chain, 1, NULL);
 	free(blockchain);
 }
