3fd08ff37087b41d7719a5b297a00e42ec5edd39
crasride <jose.dfernandez@yahoo.fr>
2024-03-28 11:16:41 +0100

    update

--- blockchain/v0.3/blockchain.h
+++ blockchain/v0.3/blockchain.h
@@ -19,7 +19,7 @@
 #define GENESIS_HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
 #define SHA256_DIGEST_LENGTH 32
 
-#include "transaction/transaction.h"
+#include "./transaction/transaction.h"
 #include "../../crypto/hblk_crypto.h"
 
 /* Constants */
