09494d87dd4fa96e5330e83ef74bef546aa3d6f2
crasride <jose.dfernandez@yahoo.fr>
2024-03-28 09:48:01 +0100

    task14 add comment

--- blockchain/v0.3/transaction/update_unspent.c
+++ blockchain/v0.3/transaction/update_unspent.c
@@ -8,8 +8,10 @@
 **/
 int find_unspent(llist_node_t node, void *hash)
 {
+	/* Unspent transaction output */
 	unspent_tx_out_t *unspent = (unspent_tx_out_t *)node;
 
+	/* Compare the hash of the unspent transaction output */
 	return (!memcmp(unspent->out.hash, hash, sizeof(unspent->out.hash)));
 }
 
@@ -23,27 +25,38 @@ int find_unspent(llist_node_t node, void *hash)
 llist_t *update_unspent(llist_t *transactions,
 				uint8_t block_hash[SHA256_DIGEST_LENGTH], llist_t *all_unspent)
 {
-	int i, j, buffer, size;
-	transaction_t *transaction;
-	tx_in_t *in;
-	tx_out_t *out;
-	unspent_tx_out_t *unspent;
+	int tr_index; /* Transaction index */
+	int io_index; /* Input-output iterate transaction*/
+	int io_count; /* Input-output count transaction*/
+	int tr_count; /* Transaction count */
+	transaction_t *transaction; /* Transaction */
+	tx_in_t *in; /* Transaction input */
+	tx_out_t *out; /* Transaction output */
+	unspent_tx_out_t *unspent; /* Unspent transaction output */
 
+	/* Check if the arguments are valid */
 	if (!transactions || !block_hash || !all_unspent)
 		return (all_unspent);
-
-	for (i = 0, size = llist_size(transactions); i < size; i++)
+	/* Loop through all transactions */
+	for (tr_index = 0, tr_count = llist_size(transactions); tr_index < tr_count;
+			tr_index++)
 	{
-		transaction = llist_get_node_at(transactions, i);
-		for (j = 0, buffer = llist_size(transaction->inputs); j < buffer; j++)
+		transaction = llist_get_node_at(transactions, tr_index);
+		/* Loop through all inputs and outputs */
+		for (io_index = 0, io_count = llist_size(transaction->inputs);
+			io_index < io_count; io_index++)
 		{
-			in = llist_get_node_at(transaction->inputs, j);
+			in = llist_get_node_at(transaction->inputs, io_index);
+			/* Remove the unspent transaction output */
 			llist_remove_node(all_unspent, find_unspent, in->tx_out_hash, 1, NULL);
 		}
-		for (j = 0, buffer = llist_size(transaction->outputs); j < buffer; j++)
-		{
-			out = llist_get_node_at(transaction->outputs, j);
+		/* Loop through all outputs */
+		for (io_index = 0, io_count = llist_size(transaction->outputs);
+			io_index < io_count; io_index++)
+		{/* Create a new unspent transaction output */
+			out = llist_get_node_at(transaction->outputs, io_index);
 			unspent = unspent_tx_out_create(block_hash, transaction->id, out);
+			/* Add the unspent transaction output */
 			llist_add_node(all_unspent, unspent, ADD_NODE_REAR);
 		}
 	}
