88d0bfd752fd64cd3aa25385409f4c0074767261
crasride <jose.dfernandez@yahoo.fr>
2024-04-12 17:15:02 +0200

    update

--- cli/cli.c
+++ cli/cli.c
@@ -1,5 +1,5 @@
 #include "cli.h"
-
+#include "../blockchain/v0.3/transaction/transaction.h"
 
 /**
 * handle_exit - handle the exit command
@@ -47,6 +47,7 @@ int handle_wallet_save(EC_KEY *ec_key, char *path)
 	if (ec_save(ec_key, path))
 	{
 		printf("Save wallet (EC key pair)\n");
+		EC_KEY_free(ec_key);
 		return (0);
 	}
 	else
@@ -62,14 +63,18 @@ int handle_wallet_save(EC_KEY *ec_key, char *path)
 * @path: path to load the key pair
 * Return: 0 if success, -1 if failed
 */
-
 int handle_wallet_load(EC_KEY **ec_key, char *path)
 {
+	if (*ec_key)
+		EC_KEY_free(*ec_key);
+
 	*ec_key = ec_load(path);
 
 	if (*ec_key)
 	{
 		printf("Load wallet (EC key pair)\n");
+		/* Print <base64 encoded data> Key */
+		/* EC_KEY_print_fp(stdout, *ec_key, 0); */
 		return (0);
 	}
 	else
@@ -89,16 +94,12 @@ int handle_wallet_load(EC_KEY **ec_key, char *path)
 * @ec_key: pointer to store EC key
 * Return: 0 if ok, 1 if exit
 */
-int find_command(char *cmd, char *arg1, char *arg2, EC_KEY **ec_key)
+int find_command(char *cmd, char *arg1, char *arg2, EC_KEY **ec_key, char *receiver_address)
 {
 	if (strcmp(cmd, "exit") == 0)
-	{
 		return (handle_exit());
-	}
 	else if (strcmp(cmd, "wallet_create") == 0)
-	{
 		return (handle_wallet_create(ec_key));
-	}
 	else if (strcmp(cmd, "wallet_save") == 0)
 	{
 		if (!arg1)
@@ -107,9 +108,7 @@ int find_command(char *cmd, char *arg1, char *arg2, EC_KEY **ec_key)
 			return (-1);
 		}
 		else
-		{
 			return (handle_wallet_save(*ec_key, arg1));
-		}
 	}
 	else if (strcmp(cmd, "wallet_load") == 0)
 	{
@@ -119,8 +118,19 @@ int find_command(char *cmd, char *arg1, char *arg2, EC_KEY **ec_key)
 			return (-1);
 		}
 		else
-		{
 			return (handle_wallet_load(ec_key, arg1));
+	}
+	else if (strcmp(cmd, "send") == 0)
+	{
+		if (!arg1 || !arg2)
+		{
+			printf("Usage: send <amount> <address>\n");
+			return (-1);
+		}
+		else
+		{
+			int amount = atoi(arg1);
+			return (handle_send(amount, receiver_address, *ec_key));
 		}
 	}
 	else
@@ -128,7 +138,168 @@ int find_command(char *cmd, char *arg1, char *arg2, EC_KEY **ec_key)
 		printf("unknown command: \"%s\"\n", cmd);
 		return (-1);
 	}
-	(void)arg2;
+}
+
+/**
+* pubKeyHexToByteArray - converts a EC_KEY public key in a lowercase or
+* uppercase hex string to a byte array
+*
+* @address: public key of EC_PUB_LEN bytes, in hex
+*
+* Return: byte array on success, or NULL on failure
+*/
+uint8_t *pubKeyHexToByteArray(const char *address)
+{
+	uint8_t *pub;
+	unsigned int scan;
+	size_t i, j, len, upper, case_detected;
+
+	if (!address)
+	{
+		fprintf(stderr, "pubKeyHexToByteArray: NULL parameter\n");
+		return (NULL);
+	}
+
+	pub = calloc(EC_PUB_LEN, sizeof(uint8_t));
+	if (!pub)
+	{
+		fprintf(stderr, "pubKeyHexToByteArray: calloc failure\n");
+		return (NULL);
+	}
+
+	len = strlen(address);
+	if (len < (EC_PUB_LEN * 2))
+	{
+		free(pub);
+		return (NULL);
+	}
+
+	for (i = 0, case_detected = 0; i < len; i++)
+	{
+		if (isdigit(address[i]))
+			continue;
+		else if (address[i] >= 'a' && address[i] <= 'f')
+		{
+			if (!case_detected)
+			{
+				upper = 0;
+				case_detected = 1;
+			}
+			if (upper)
+			{
+				free(pub);
+				return (NULL);
+			}
+		}
+		else if (address[i] >= 'A' && address[i] <= 'F')
+		{
+			if (!case_detected)
+			{
+				upper = 1;
+				case_detected = 1;
+			}
+			if (!upper)
+			{
+				free(pub);
+				return (NULL);
+			}
+		}
+		else
+		{
+			free(pub);
+			return (NULL);
+		}
+
+	}
+
+	for (i = 0, j = 0; i < EC_PUB_LEN; i++, j += 2)
+	{
+		if (sscanf(address + j, upper ? "%02X" : "%02x", &scan) == -1)
+		{
+			perror("pubKeyHexToByteArray: sscanf");
+			free(pub);
+			return (NULL);
+		}
+		pub[i] = scan;
+	}
+
+	return (pub);
+}
+
+
+
+/**
+* handle_send - handle the send command
+* @amount: amount to send
+* @receiver_address: receiver address
+* @sender_key: sender key
+* Return: 0 if success, -1 if failed
+*/
+int handle_send(int amount, const char *receiver_address, EC_KEY *sender_key)
+{
+	uint8_t *pub;
+	EC_KEY *receiver_key = NULL;
+	transaction_t *tx = NULL;
+	llist_t *transaction_pool = NULL;
+
+	pub = pubKeyHexToByteArray(receiver_address);
+
+	if (!pub)
+	{
+		printf("Failed to convert receiver address\n");
+		return -1;
+	}
+
+	receiver_key = ec_from_pub(pub);
+	if (!receiver_key)
+	{
+		printf("Failed to create receiver key\n");
+		free(pub);
+		return -1;
+	}
+
+	tx = transaction_create(sender_key, receiver_key, amount, NULL);
+	if (!tx)
+	{
+		printf("Failed to create transaction\n");
+		EC_KEY_free(receiver_key);
+		free(pub);
+		return -1;
+	}
+
+	if (!transaction_is_valid(tx, NULL))
+	{
+		printf("Invalid transaction\n");
+		transaction_destroy(tx);
+		EC_KEY_free(receiver_key);
+		free(pub);
+		return -1;
+	}
+
+	transaction_pool = llist_create(MT_SUPPORT_FALSE);
+	if (!transaction_pool)
+	{
+		printf("Failed to create transaction pool\n");
+		transaction_destroy(tx);
+		EC_KEY_free(receiver_key);
+		free(pub);
+		return -1;
+	}
+
+	if (llist_add_node(transaction_pool, tx, ADD_NODE_REAR) == -1)
+	{
+		printf("Failed to add transaction to pool\n");
+		transaction_destroy(tx);
+		EC_KEY_free(receiver_key);
+		free(pub);
+		return -1;
+	}
+
+	printf("Transaction sent successfully\n");
+
+	EC_KEY_free(receiver_key);
+	free(pub);
+	return 0;
 }
 
 /**
@@ -141,7 +312,6 @@ void cleanup(EC_KEY *ec_key)
 		EC_KEY_free(ec_key);
 }
 
-
 /**
 * main - Entry point
 * Return: 0 if ok, 1 if failed
@@ -152,7 +322,8 @@ int main(void)
 	ssize_t read;
 	size_t len;
 	int ret = 0;
-	char *line = NULL, *cmd, *arg1, *arg2;
+	char *line = NULL, *cmd, *arg1, *arg2, receiver_address[ADDRESS_MAX_LEN];;
+
 
 	while (1)
 	{
@@ -167,13 +338,12 @@ int main(void)
 		arg1 = strtok(NULL, " ");
 		arg2 = strtok(NULL, " ");
 
-		ret = find_command(cmd, arg1, arg2, &ec_key);
+		ret = find_command(cmd, arg1, arg2, &ec_key, receiver_address);
 		if (ret == 1)
 			break;
 	}
 	if (line)
 		free(line);
 	cleanup(ec_key);
-
 	return (ret != 0);
 }
--- cli/cli.h
+++ cli/cli.h
@@ -9,17 +9,23 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <ctype.h>
 
 
 #include "../crypto/hblk_crypto.h"
 #include "../blockchain/v0.3/blockchain.h"
 #include "../blockchain/v0.3/transaction/transaction.h"
 
+#define ADDRESS_MAX_LEN 42
+
 /* Prototypes Functions */
 int handle_exit(void);
 int handle_wallet_create(EC_KEY **ec_key);
 int handle_wallet_save(EC_KEY *ec_key, char *path);
 int handle_wallet_load(EC_KEY **ec_key, char *path);
+int handle_send(int amount, const char *receiver_address, EC_KEY *sender_key);
+uint8_t *pubKeyHexToByteArray(const char *address);
+
 
 
 #endif /* _CLI_H_ */
